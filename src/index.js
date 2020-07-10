c13a75bb242be1d269a54106812e1d81dc81313a
Pratyush2710 <pratyushhg@gmail.com>
2020-07-10 17:52:46 +0530

    Mini book project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//stateless functional component
+//ES6, Nested Components
+function Books() {
+  return (
+    <section className="books">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+//component with var and implicit return
+const Book = () => {
+  return (
+    <article className="book">
+      <CoverImage />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const CoverImage = () => (
+  <img
+    width="200"
+    src="https://images-eu.ssl-images-amazon.com/images/I/814L%2Bvq01mL._AC_UL200_SR200,200_.jpg"
+    alt="IKIGAI"
+  />
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Title with inline CSS defined by {{}}
+const authorStyle = {
+  letterSpacing: "2px",
+  color: "green",
+};
+const Title = () => <h1 style={{ fontSize: "2rem", color: "red" }}>IKIGAI</h1>;
+const Author = () => <p style={authorStyle}> by Pratyush Garg </p>;
+
+ReactDOM.render(<Books />, document.getElementById("root"));
