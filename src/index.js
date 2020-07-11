93523871b5082f77328a3ebedc8b0dd3007f0a8d
Pratyush2710 <pratyushhg@gmail.com>
2020-07-11 23:15:35 +0530

    Mini book project replaced with props and children

--- src/index.js
+++ src/index.js
@@ -4,41 +4,40 @@ import "./index.css";
 
 //stateless functional component
 //ES6, Nested Components
-function Books() {
+
+function People() {
+  const friends = [
+    { name: "Pratyush", job: "developer", age: 22, company: "Jio" },
+    { name: "Shatakshi", job: "developer", age: 22, company: "Jio" },
+    { name: "Himanshi", job: "developer", age: 22, company: "Jio" },
+  ];
+
   return (
-    <section className="books">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section>
+      <Person person={friends[0]}>
+        <p>Some info abiut me</p>
+      </Person>
+      <Person person={friends[1]} />
+      <Person person={friends[2]} />
     </section>
   );
 }
-//component with var and implicit return
-const Book = () => {
+
+//props (Properties) parameter passesd
+//destructured props
+const Person = ({ children, person: { name, job, age, company } }) => {
+  // const { name, job, age, company } = props.person;
+  //props.person - Keyword
   return (
-    <article className="book">
-      <CoverImage />
-      <Title />
-      <Author />
+    <article>
+      <h1>{name}</h1>
+      {children}
+      <p>{job}</p>
+      <p>{age}</p>
+      <p>{company}</p>
+      <hr />
     </article>
   );
 };
-const CoverImage = () => (
-  <img
-    width="200"
-    src="https://www.amazon.in/images/I/51useSyby-L._SX323_BO1,204,203,200_.jpg"
-    alt="The ORIGIN"
-  />
-);
 
-//Title with inline CSS defined by {{}}
-const authorStyle = {
-  letterSpacing: "2px",
-  color: "green",
-};
-const Title = () => <h1 style={{ fontSize: "2rem", color: "red" }}>ORIGIN</h1>;
-const Author = () => <p style={authorStyle}> by Dan Brown </p>;
-
-ReactDOM.render(<Books />, document.getElementById("root"));
+ReactDOM.render(<People />, document.getElementById("root"));
